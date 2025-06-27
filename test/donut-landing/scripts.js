7f50f11f8f7b50922e9c02ac2b59c522ce0b8dc6
Bato Web Agency <91454069+bato-web-agency@users.noreply.github.com>
2025-06-27 09:37:12 +0300

    Add files via upload

--- test/donut-landing/scripts.js
+++ test/donut-landing/scripts.js
@@ -59,7 +59,7 @@ loader.load('./models/drip_donut.glb', (gltf) => {
 	model = gltf.scene;
 
 	model.children[0].children[0].children[0].children[0].children[1].children[0].children[0].material.color.set('#ffc6f3');
-	model.rotation.set(0, 1.5, 1.5);
+	model.rotation.set(0, 1.5, 0.75);
 	scene.add(model);
 
 	setupGsapAnimations(model);
@@ -81,16 +81,17 @@ const handleResponsive = () => {
 	if (!model) return;
 
 	const dots = [
-		{ width: 1300, scale: 1, position: [0.15, 0, -2], z: 5 },
+		{ width: 1300, scale: 1, position: [0, -0.1, -2], z: 5 },
 		{ width: 1280, scale: 1, position: [0.2, 0, -2], z: 5 },
 		{ width: 1200, scale: 1, position: [0, 0, -2], z: 5 },
 		{ width: 1024, scale: 1, position: [0.5, 0, -3], z: 5.5 },
 		{ width: 960, scale: 1, position: [0.5, 0, -3], z: 6 },
 		{ width: 768, scale: 0.7, position: [0.5, 0, -1.2], z: 6.5 },
 		{ width: 640, scale: 0.7, position: [0.45, 0, -1.2], z: 7 },
-		{ width: 575, scale: 0.5, position: [0.35, 0.25, -1.2], z: 7 },
-		{ width: 375, scale: 0.35, position: [0.25, 0.2, -1.2], z: 7.5 },
-		{ width: 0, scale: 0.35, position: [0.25, 0.2, -1.2], z: 7.5 }
+		{ width: 575, scale: 0.5, position: [0.45, -0.1, -1.2], z: 7 },
+		{ width: 475, scale: 0.5, position: [0.3, -0.1, -1.2], z: 7 },
+		{ width: 375, scale: 0.35, position: [0.3, 0, -1.2], z: 7.5 },
+		{ width: 0, scale: 0.35, position: [0.25, -0.1, -1.2], z: 7.5 }
 	];
 
 	const dot = dots.find(d => width >= d.width) || dots[dots.length - 1];
@@ -161,7 +162,7 @@ const setupGsapAnimations = (model) => {
 		.to(model.rotation, { x: 0, z: 0.8, duration: 1 })
 		.to(model.scale, { x: window.innerWidth < 575 ? 1.75 : 2, y: window.innerWidth < 575 ? 1.75 : 2, z: window.innerWidth < 575 ? 1.75 : 2, duration: 1,}, '<')
 		.to(model.position, { x: 0, y: 0, z: -5, duration: 1 }, '<')
-		.to(modelContainer, { x: '0vw', y: window.innerWidth < 1024 ? '10vh' : '20vh', duration: 1 }, '<+=0.1');
+		.to(modelContainer, { x: '0vw', y: window.innerWidth < 1024 ? '20vh' : '20vh', duration: 1 }, '<+=0.1');
 
 	// Flip Section Animations
 
@@ -172,7 +173,7 @@ const setupGsapAnimations = (model) => {
 			trigger: '#flip',
 			start: 'top top',
 			end: () => `+=${window.innerHeight}px`,
-			scrub: 1.5,
+			scrub: 1,
 			pin: true,
 			pinSpacing: false,
 		}
@@ -191,7 +192,6 @@ const setupGsapAnimations = (model) => {
 	const cardsArray = Array.from(cards);
 
 	const colors = [
-		{ r: 0.62, g: 0.80, b: 0.39 },
 		{ r: 0.9568627450980393, g: 0.9254901960784314, b: 0.8666666666666667 },
 		{ r: 0.9098039215686274, g: 0.5490196078431373, b: 0.7333333333333333 },
 		{ r: 0.16862745098039217, g: 0.047058823529411764, b: 0.011764705882352941 },
@@ -201,7 +201,7 @@ const setupGsapAnimations = (model) => {
 		scrollTrigger: {
 			trigger: '#steps',
 			start: 'top top',
-			end: () => `+=${window.innerHeight * 4}`,
+			end: () => `+=${window.innerHeight * 3}`,
 			scrub: true,
 			pin: true,
 			pinSpacing: false
@@ -209,30 +209,107 @@ const setupGsapAnimations = (model) => {
 	});
 
 	timeline4
-		.to(modelContainer, { x: window.innerWidth < 1200 ? '25vw' : 0,  y: window.innerWidth < 1200 ? '32vh' : '28vh',  duration: 1 }, 0)
-		.to(model.scale, { x: window.innerWidth < 768 ? 1.2 : 2,  y: window.innerWidth < 768 ? 1.2 : 2,  z: window.innerWidth < 768 ? 1.2 : 2,  duration: 1}, '<')
-		.to('.steps__heading', {opacity: 1, y: 0, duration: 0.5}, '<')
+		.to(modelContainer, {
+			x: () => {
+				if (window.innerHeight < 600 && window.innerWidth < 768) return '0vw';
+				return window.innerWidth < 768
+					? '0vw'
+					: window.innerWidth < 1200
+						? '0vw'
+						: '-2.5vw';
+			},
+			y: () => {
+				return window.innerWidth < 768
+					? '25vh'
+					: '5vh'
+			},
+			duration: 1
+		}, 0)
+		.to(model.scale, { x: window.innerWidth < 1300 ? 1.5 : 2.25,  y: window.innerWidth < 1300 ? 1.5 : 2.25,  z: window.innerWidth < 1300 ? 1.5 : 2.25,  duration: 1}, '<')
+		.to('.steps__heading', {opacity: 1, y: 0, duration: 0.5}, '<');
 
 	cardsArray.forEach((card, index) => {
-		const y = `-${(index + 1) * 50}vh`;
+		const y = `-${(index + 1) * 100}vh`;
 		const color = colors[index % colors.length];
 		const stepTime = (index + 1) * 5;
+		const delayTime = stepTime + 0.3;
 
 		timeline4
 			.to(card, { y: y, duration: 5, ease: 'power3.out' }, stepTime)
-			.to(donutTop.color, {r: color.r, g: color.g, b: color.b, duration: 5, ease: 'power3.out' }, stepTime - 3);
+			.to(donutTop.color, {r: color.r, g: color.g, b: color.b, duration: 5, ease: 'power3.out' }, stepTime - 3)
+			.to(model.rotation, {
+				y: "+=" + Math.PI * 2,
+				duration: 5,
+				ease: "power2.out"
+			}, delayTime - 3)
 
-		if (index === 3) {
+
+		if (index === 2) {
 			timeline4.to(donutParticles.material, { opacity: 0, duration: 3, ease: 'power2.out' }, stepTime);
 		}
+	});
 
+	// Numbers Section Animations
+
+	const items = gsap.utils.toArray('.numbers__item');
+
+	const timeline5 = gsap.timeline({
+		scrollTrigger: {
+			trigger: '#numbers',
+			start: 'top top',
+			end: () => `+=${window.innerHeight * items.length - 2}`,
+			scrub: true,
+			pin: true,
+			pinSpacing: false,
+		}
+	});
+
+	timeline5
+		.to(donutTop.color, { r: 0.9098039215686274, g: 0.5490196078431373, b: 0.7333333333333333, duration: 5, ease: 'power3.out' }, '<')
+		.to(donutParticles.material, { opacity: 1, duration: 1, ease: 'power2.out' }, '<')
+		.to(model.scale, {
+			x: (window.innerHeight < 600 || window.innerWidth < 1024) ? 1.5 : 2.25,
+			y: (window.innerHeight < 600 || window.innerWidth < 1024) ? 1.5 : 2.25,
+			z: (window.innerHeight < 600 || window.innerWidth < 1024) ? 1.5 : 2.25,
+			duration: 1
+		}, '<')
+		.to(modelContainer, {
+			x: () => {
+				if (window.innerWidth < 640) return '0vw';
+				if (window.innerHeight < 600) return '0';
+				return '25vw';
+			},
+			y: () => {
+				if (window.innerHeight < 600) return '20vh';
+				if (window.innerWidth < 640) return '30vh';
+				return window.innerWidth < 1200 ? '5vh' : '15vh';
+			},
+			duration: 1
+		}, '<')
+		.to('.numbers__heading', {opacity: 1, y: 0, duration: 1}, '<');
+
+	items.forEach((item, i) => {
+		const others = items.filter((item, index) => index !== i);
+
+		timeline5
+			.to(others, {
+				opacity: 0.3,
+				duration: 0.5,
+				ease: 'power1.out'
+			}, i)
+
+			.to(item, {
+				opacity: 1,
+				duration: 1,
+				ease: 'power2.out'
+			}, i);
 	});
 
 	// Final Section Animations
 
 	const finalText = document.querySelector('.final__order-box');
 
-	const timeline5 = gsap.timeline({
+	const timeline6 = gsap.timeline({
 		scrollTrigger: {
 			trigger: '#final',
 			start: 'top-=400 top',
@@ -243,28 +320,25 @@ const setupGsapAnimations = (model) => {
 			},
 			onLeaveBack: () => {
 				controlModel = false;
-				gsap.to(model.rotation, {x: 0, y: 1.5, z: 0.8, duration: 1});
 				gsap.to(model.position, {x: 0, y: -0.01, z: -5, duration: 1});
-				gsap.to(model.scale, {x: window.innerWidth < 768 ? 1.25 : 2, y: window.innerWidth < 768 ? 1.25 : 2, z: window.innerWidth < 768 ? 1.25 : 2, duration: 1,});
+				gsap.to(model.scale, {x: window.innerWidth < 1024 ? 1.5 : 2.25, y: window.innerWidth < 1024 ? 1.5 : 2.25, z: window.innerWidth < 1024 ? 1.5 : 2.25, duration: 1,});
 			}
 		}
 	});
 
-	timeline5
-		.to('.final__heading', { opacity: 1, y: 0, duration: 0.1}, '<')
+	timeline6
+		.to('.final__order-box', { opacity: 1, y: 0, duration: 0.5}, '<')
 		.to(donutTop.color, { r: 0.62, g: 0.80, b: 0.39, duration: 0.5 }, '<')
 		.to(donutParticles.material, { opacity: 1, duration: 1, ease: 'power2.out' }, '<')
 		.to(modelContainer, {
-			x: window.innerHeight < 500
-				? '-15vw' : (window.innerWidth < 575 ? '-5vw' : '-30vw'),
+			x: window.innerWidth < 768
+				? '0vw' : (window.innerWidth < 575 ? '-5vw' : '-20vw'),
 			y: window.innerHeight < 500
 				? '25vh'
-				: (window.innerWidth < 768 ? '-15vh' : '10vh'),
+				: (window.innerWidth < 1200 ? '30vh' : '5vh'),
 			duration: 1
 		}, '<')
-		.to(model.rotation, { y: 1.5, duration: 1 }, '<')
-		.to(model.scale, { x: window.innerWidth < 1024 ? 1.8 : 2, y: window.innerWidth < 1024 ? 1.8 : 2, z: window.innerWidth < 1024 ? 1.8 : 2, duration: 1,}, '<')
-
+		.to(model.scale, { x: window.innerWidth < 1024 ? 1.5 : 2.25, y: window.innerWidth < 1024 ? 1.5 : 2.25, z: window.innerWidth < 1024 ? 1.5 : 2.25, duration: 1,}, '<')
 		.to(finalText, { opacity: 1, y: 0, duration: 0.5 }, '<');
 }
 
@@ -293,6 +367,42 @@ window.addEventListener('mousemove', (event) => {
 	}
 
 	prevMousePosition = { x: event.clientX, y: event.clientY };
+});
+
+window.addEventListener('touchstart', (event) => {
+	if (event.touches.length === 1) {
+		isDragging = true;
+		prevMousePosition = {
+			x: event.touches[0].clientX,
+			y: event.touches[0].clientY
+		};
+	}
+});
+
+window.addEventListener('touchmove', (event) => {
+	if (!isDragging || !controlModel || event.touches.length !== 1) return;
+
+	const touch = event.touches[0];
+	const mouseMove = {
+		x: touch.clientX - prevMousePosition.x,
+		y: touch.clientY - prevMousePosition.y
+	};
+
+	if (model) {
+		model.rotation.y += mouseMove.x * 0.0025;
+		model.rotation.x += mouseMove.y * 0.0025;
+	}
+
+	prevMousePosition = {
+		x: touch.clientX,
+		y: touch.clientY
+	};
+
+	event.preventDefault();
+}, { passive: false });
+
+window.addEventListener('touchend', () => {
+	isDragging = false;
 });
 
 window.addEventListener('mouseup', () => {
